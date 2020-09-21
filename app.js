cc008c4aa3420e2a762f5f6b9632977f74565125
dormarble <gudfla0814@gmail.com>
2020-09-22 00:36:31 +0900

    move connection.js to models/index.js

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
-import config from './config'
-import db, { connectDB } from './connection'
-import { Complex } from './model/Complex'
-import { Pyeong } from './model/Pyeong'
+import db from './model'
 
-connectDB(config.development)
 const connection = db.conn
 
 // force: true 서버실행 시마다 테이블을 재생성
--- model/index.js
+++ model/index.js
@@ -1,6 +1,18 @@
-import defineComplex from './Complex'
-import definePyeong from './Pyeong'
+import Sequelize from 'sequelize'
 
-export default function() {
-    
-}
+import config from '../config'
+
+const db = {}
+
+const dbConfig = config.development
+db.conn = new Sequelize(
+    dbConfig.dbName, 
+    dbConfig.dbUser, 
+    dbConfig.dbPwd, 
+    {
+        host: dbConfig.dbHost,
+        dialect: dbConfig.dbEngine
+    }
+)
+
+export default db
