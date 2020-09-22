379c7692eaf002d17d6929287fcdd0a092bf2f66
dormarble <gudfla0814@gmail.com>
2020-09-23 01:12:24 +0900

    install express

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
+import express from 'express'
+
 import db from './model'
+import config from './config'
+
+const PORT = config.development.port || 3000
+
+const app = express()
 
 // force: true 서버실행 시마다 테이블을 재생성
 db.conn.sync({force: false})
-.then(() => console.log("connect database successfully"))
+.then(() => {
+    console.log("connect database successfully")
+    app.listen(PORT, () => console.log("express is running"))
+})
 .catch(err => {console.log("failed to connect database"); console.log(err)})
 
 db.Complex.findAndCountAll().then(data => {
