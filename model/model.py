e596de4298049c8d14d50dbba538de3896f9b948
Dormar <gudfla0814@gmail.com>
2020-11-23 04:41:38 +0900

    modify code

--- model/model.py
+++ model/model.py
@@ -11,7 +11,7 @@ class Model:
         self.random_forest_regressor = joblib.load('./random_forest_regressor.pkl') 
         self.decision_tree_regressor = joblib.load('./decision_tree_regressor.pkl')
         self.xgboost_regressor = joblib.load('./xgboost_regressor.pkl')
-        self.meta_regressor = joblib.load('./metaModel.pkl')
+        self.meta_regressor = joblib.load('./meta_model.pkl')
     
     def predict_child_model(self, input):
         rf = self.random_forest_regressor.predict(input)
